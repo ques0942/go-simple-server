f6738abbbef538c92b6840579248ade9eda60f6d
t-naritomi <nari0942@gmail.com>
2019-02-20 19:34:32 +0900

    add sleep

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
+		time.Sleep(1 * time.Second)
 		c.String(200, "OK")
 	})
 	r.Run()
